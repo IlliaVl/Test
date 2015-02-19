2ce3ed8f74e3c1b051ad98832632ae816ed71dd0
Vlasov Illia <vlasovia@gmail.com>
2015-02-19 20:04:20 +0200

    First commit

---
+++ Ateamo/app/src/main/java/com/ateamo/ateamo/AteamoRestClient.java
@@ -0,0 +1,25 @@
+package com.ateamo.ateamo;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+/**
+ * Created by vlasovia on 19.02.15.
+ */
+public class AteamoRestClient {
+    private static final String BASE_URL = "https://api.ateamo.com";
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+}
